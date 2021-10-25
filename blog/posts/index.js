4cf51ebf0200acf40b08b8f69b87760a5e52618d
Marcelo Carbonera <marcelocarbonera@live.com>
2021-10-25 16:25:20 -0300

    Docker and Kubernetes

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -9,11 +9,11 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-});
+//app.get('/posts', (req, res) => {
+//    res.send(posts)
+//});
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -21,7 +21,7 @@ app.post('/posts', async (req, res) => {
         id, title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
